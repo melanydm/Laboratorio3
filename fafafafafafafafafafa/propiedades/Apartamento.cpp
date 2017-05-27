2cc623f5297f0014654839b3ce425cf53040a2ff
caralnuma <caralnuma@gmail.com>
2017-05-26 22:29:28 -0300

    CAgadas las clases tienen mal los nombres
    No empiezan en mayuscula

---
+++ fafafafafafafafafafa/propiedades/Apartamento.cpp
@@ -0,0 +1,17 @@
+#include "Propiedad.hpp"
+#include "Apartamento.hpp"
+
+apartamento::apartamento():propiedad()
+{
+    //ctor
+}
+
+apartamento::apartamento(DtApartamento apto):propiedad(apto)
+{
+    //ctor
+}
+
+apartamento::~apartamento()
+{
+    //dtor
+}
---
+++ fafafafafafafafafafa/propiedades/Apartamento.hpp
@@ -0,0 +1,17 @@
+#ifndef APARTAMENTO_HPP
+#define APARTAMENTO_HPP
+
+#include <propiedad.hpp>
+
+
+class apartamento : public propiedad
+{
+    public:
+        apartamento();
+        apartamento(DtApartamento);
+        virtual ~apartamento();
+    protected:
+    private:
+};
+
+#endif // APARTAMENTO_HPP
