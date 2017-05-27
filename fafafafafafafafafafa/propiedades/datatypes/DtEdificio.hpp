380428532b07528334717ae088387f5c52dea8ab
Fernando <nandomadriaga@gmail.com>
2017-05-26 21:11:10 -0300

    Update DtEdificio.hpp

--- fafafafafafafafafafa/propiedades/datatypes/DtEdificio.hpp
+++ fafafafafafafafafafa/propiedades/datatypes/DtEdificio.hpp
@@ -10,14 +10,24 @@ class DtEdificio  {
 
 		// Operators
 
-		virtual bool operator == (DtEdificio) = 0;
-
+		//virtual bool operator == (DtEdificio) = 0;
+		
+		// Getters
+		string getNombre();
+		int getCantPisos();
+		float getGastosComunes();
+	
+		// Setters
+		void setNombre(string);
+		void setCantPisos(int);
+		void setGastosComunes(float);
+	
 		// Destructor
 
 		virtual ~DtEdificio();
 }
 
-virtual istream& operator>>(istream&, DtEdificio);
-virtual ostream& operator<<(ostream&, DtEdificio);
+//istream& operator>>(istream&, DtEdificio);
+//ostream& operator<<(ostream&, DtEdificio);
 
 #endif //DT_EDIFICIO_HPP
