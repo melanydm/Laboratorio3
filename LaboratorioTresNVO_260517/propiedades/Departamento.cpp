f77e68e1a947c9815a961e37578bff38cc3aca2e
caralnuma <caralnuma@gmail.com>
2017-05-28 16:54:19 -0300

    Errores con setters y getters

--- LaboratorioTresNVO_260517/propiedades/Departamento.cpp
+++ LaboratorioTresNVO_260517/propiedades/Departamento.cpp
@@ -13,6 +13,10 @@ Departamento::Departamento(DtDepartamento depto)
     this.nombre=depto.getNombre();
 }
 
+string Departamento::getId() { return id; }
+void Departamento:: setId(string val) { id = val; }
+string Departamento::getNombre() { return nombre; }
+void Departamento::setNombre(string val) { nombre = val; }
 
 Departamento::~Departamento()
 {
