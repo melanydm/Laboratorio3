38ef13e9c391f813e5d954167463933d08459357
dbarreto22 <ddiegob2212@gmail.com>
2017-05-31 20:04:03 -0300

    cin y out

--- LaboratorioTresNVO_260517/propiedades/datatypes/DtEdificio.cpp
+++ LaboratorioTresNVO_260517/propiedades/datatypes/DtEdificio.cpp
@@ -30,37 +30,34 @@ void DtEdificio::setGastos_Comunes(string gastos_comunes) { this->gastos_Comunes
 
 istream& operator>>(istream& in, DtEdificio& dte){
 	string nombre = "";
-	string cant_pisos = 0;
-	string gastos_comunes = 0;
+	int cant_pisos = 0;
+	float gastos_comunes = 0;
+
 	int i = 0;
 	string s = " ";
-	while ( s != "\n"  && i < 3 ){
+	while ( s != "\n"  && i < 4){
 		in >> setw(1) >> s;
 		if (s == ",")
 			i++;
 		if (i == 0)
 			nombre = nombre + s;
 		else if (i == 1)
-        {
-            cant_pisos = cant_pisos + s;
-        }
-
+			cant_pisos = cant_pisos * 10 + (int)s;
 		else if (i == 2)
-			gastos_comunes = gastos_comunes + s;
+			gastos_comunes = gastos_comunes * 10 + (float)s;
 			// en este caso habrá que ver los decimales.
 	}
-
-	dte.setNombre(nombre);
-	dte.setCant_Pisos(cant_pisos);
-	dte.setGastos_Comunes(gastos_comunes);
-	return in;
+	
+	dte->setNombre(nombre);
+	dte->setCantPisos(cant_pisos) = cant_pisos;
+	dte->setGastosComunes(gastos_comunes) = gastos_comunes;
 }
 
 // Sobrecarga cout
 ostream& operator<<(ostream& out, DtEdificio& dte){
-    out<< "Nombre:" << dte.getNombre() << "\r\n"
-	<< "Cant. pisos:" << dte.getCant_Pisos() << "\r\n"
-	<< "Gastos comunes:" << dte.getGastos_Comunes();
+    out << "Nombre:" << this->nombre << "\r\n"
+	<< "Cant. pisos:" << this->cant_pisos << "\r\n"
+	<< "Gastos comunes:" << this->gastos_comunes;
     return out;
 }
 
